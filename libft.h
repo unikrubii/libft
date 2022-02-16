d2bfc25aa3d4b64ba3e55ee70915fe9bbcf36a27
unikrubii <gamez.zeta@gmail.com>
2022-02-16 22:20:47 +0700

    libft fixed def

--- libft.h
+++ libft.h
@@ -10,5 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
+
 int ft_isalpha(int  c);
 int ft_isalpha(int  c);
+
+#endif
