d21c4f4cc52d53c59525d563caa80f40e7d8eaeb
unikrubii <gamez.zeta@gmail.com>
2022-02-18 01:26:08 +0700

    fix libft

--- libft.h
+++ libft.h
@@ -24,7 +24,7 @@ int ft_toupper(int c);
 int ft_tolower(int c);
 int ft_atoi(const char *str);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
-int strncmp(const char *s1, const char *s2, size_t n);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 char    *strchr(const char *s, int c);
 char    *strrchr(const char *s, int c);
 void    *ft_memset(void *b, int c, size_t len);
