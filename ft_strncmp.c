25c4452635f473afec058a6897a159724dc1fed6
unikrubii <gamez.zeta@gmail.com>
2022-02-18 01:41:13 +0700

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,13 +16,15 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
     size_t  i;
 
+    if (n == 0)
+        return (0);
     i = 0;
     while (i < n)
     {
-        if ((*s1 + i) == (*s2 + i))
+        while (s1[i] && s1[i] == s2[i])
             i++;
         else
-            return (*s1 - *s2);
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
     }
     return (0);
 }
