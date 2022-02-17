e89a3ca20d0e0e60fa25aaa532e2c647b2b58817
unikrubii <gamez.zeta@gmail.com>
2022-02-18 02:03:51 +0700

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,11 +14,11 @@ char    *ft_strchr(const char *s, int c)
 {
     while (*s)
     {
-        if (*s == c)
+        if (*s == (unsigned char)c)
             return ((char *)s);
         s++;
     }
-    if (c == '\0')
+    if ((unsigned char)c == '\0')
         return ((char *)s);
     return (0);
 }
