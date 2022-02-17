e89ca941ce0ff53d3b6ea7b8d51f6ee1129dc6f3
rubii <gamez.zeta@gmail.com>
2022-02-17 15:46:11 +0000

    memcmp fixed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,18 +12,18 @@
 
 #include "libft.h"
 
-int memcmp(const void *s1, const void *s2, size_t n)
+int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
     size_t  i;
 
     i = 0;
     while (i < n)
     {
-        if (*(unsigned char *)s1 + i != *(unsigned char *)s2 + i)
-        {
-            return (*((unsigned char *)s1 + i) - (*(unsigned char *)s2 + i));
-        }
-        i++;
+		if (*((unsigned char *)s1 + i) == *((unsigned char *)s2 + i))
+			i++;
+		else
+			return ((*((unsigned char *)s1 + i)) -
+						*((unsigned char *)s2 + i));
     }
     return (0);
 }
