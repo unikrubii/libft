4559166bf7ff8580f782197ddc47976f198835b5
unikrubii <gamez.zeta@gmail.com>
2022-02-18 01:50:59 +0700

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,9 +21,9 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
     i = 0;
     while (i < n)
     {
-        if (s1[i] == s2[i])
+        if (s1[i] && s1[i] == s2[i])
         {
-            while (s1[i])
+            while (s1[i] && s1[i] == s2[i] && i < n)
                 i++;
         }
         else
