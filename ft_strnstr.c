72b516fde939236c9a954dff8dbb781e55034027
unikrubii <gamez.zeta@gmail.com>
2022-02-18 19:44:23 +0700

    strnstr libft

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-char    *strnstr(const char *haystack, const char *needle, size_t len)
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     size_t  nlen;
 
--- libft.h
+++ libft.h
@@ -27,7 +27,7 @@ int ft_memcmp(const void *s1, const void *s2, size_t n);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
-char    *strnstr(const char *haystack, const char *needle, size_t len);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 void    *ft_memset(void *b, int c, size_t len);
 void    *ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
