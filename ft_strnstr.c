e6286249e2c774b722dbc8a09527c4fd1267a24f
unikrubii <gamez.zeta@gmail.com>
2022-02-18 19:38:39 +0700

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 15:56:01 by sthitiku          #+#    #+#             */
+/*   Updated: 2022/02/18 15:56:01 by sthitiku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *strnstr(const char *haystack, const char *needle, size_t len)
+{
+    size_t  nlen;
+
+    nlen = ft_strlen(needle);
+    if (!needle || needle == haystack)
+        return ((char *)haystack);
+    while (haystack && nlen < len)
+    {
+        if (*haystack == *needle && ft_strncmp(haystack, needle, nlen) == 0)
+            return ((char *)haystack);
+        haystack++;
+    }
+    return (0);
+}
