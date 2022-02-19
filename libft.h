3061aa29700f2e452b910ad695bbaec8a41d6a1a
unikrubii <gamez.zeta@gmail.com>
2022-02-20 00:14:48 +0700

    libft add ;

--- libft.h
+++ libft.h
@@ -28,7 +28,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
 char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1);
 void    *ft_memset(void *b, int c, size_t len);
 void    *ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
