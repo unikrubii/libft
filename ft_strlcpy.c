98b5b929c73fb001e6c6126fea16e823434fdc54
rubii <gamez.zeta@gmail.com>
2022-02-19 16:24:22 +0000

    strlcpy fixed

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -19,7 +19,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (!dstsize)
 		return (ft_strlen(src));
 	i = 0;
-	while (src[i] && i < (size - 1))
+	while (src[i] && i < (dstsize - 1))
 	{
 		dst[i] = src[i];
 		i++;
