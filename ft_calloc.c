19904c6e9383d9cdf14d14f8bfe5e413fdf08c24
unikrubii <gamez.zeta@gmail.com>
2022-02-20 00:06:44 +0700

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 23:35:01 by sthitiku          #+#    #+#             */
+/*   Updated: 2022/02/19 23:35:01 by sthitiku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if (count == 0 || size == 0)
+	{
+		count = 1;
+		size = 1;
+	}
+	ptr = malloc(count * size);
+	if (ptr)
+		ft_bzero(ptr, count * size);
+	return (ptr);
+}
--- libft.h
+++ libft.h
@@ -32,6 +32,7 @@ void    *ft_memset(void *b, int c, size_t len);
 void    *ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memchr(const void *s, int c, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 void    ft_bzero(void *s, size_t n);
 size_t  ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
