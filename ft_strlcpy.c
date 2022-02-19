519328f9b4a98625a89737299012104640aa82a4
unikrubii <gamez.zeta@gmail.com>
2022-02-19 23:20:59 +0700

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -14,15 +14,16 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	slen;
+	size_t	i;
 
-	slen = ft_strlen(src);
-	if (slen + 1 < dstsize)
-		ft_memcpy(dst, src, slen + 1);
-	else if (dstsize != 0)
+	if (!dstsize)
+		return (ft_strlen(src));
+	i = 0;
+	while (src[i] && i < (size - 1))
 	{
-		ft_memcpy(dst, src, slen - 1);
-		dst[dstsize - 1] = '\0';
+		dst[i] = src[i];
+		i++;
 	}
-	return (slen);
+	dst[i] = '\0';
+	return (ft_strlen(src));
 }
