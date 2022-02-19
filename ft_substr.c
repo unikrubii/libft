d10c1ba887df48d58635fcc07411ba06ba20684a
unikrubii <gamez.zeta@gmail.com>
2022-02-20 01:50:02 +0700

    substr

---
+++ ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 01:27:22 by sthitiku          #+#    #+#             */
+/*   Updated: 2022/02/20 01:27:22 by sthitiku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = s[(size_t)start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
