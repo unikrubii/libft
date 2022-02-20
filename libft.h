a4718b19f95e9a5a833c067bbae44c5cf9f0f249
unikrubii <gamez.zeta@gmail.com>
2022-02-20 18:11:57 +0700

    putnbr_fd

--- libft.h
+++ libft.h
@@ -41,6 +41,7 @@ void	ft_bzero(void *s, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
