d57aad6a517546a55fe480ba9bcb039866ba0473
unikrubii <gamez.zeta@gmail.com>
2022-02-20 15:49:13 +0700

    strjoin fixed joining

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -23,10 +23,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s3 = (char *)malloc(sizeof(char) * (len + 1));
 	i = 0;
 	j = 0;
-	while (i <= ft_strlen(s1))
+	while (s1[j])
 		s3[i++] = s1[j++];
 	j = 0;
-	while (i <= ft_strlen(s2))
+	while (s2[j])
 		s3[i++] = s2[j++];
 	s3[i] = '\0';
 	return (s3);
