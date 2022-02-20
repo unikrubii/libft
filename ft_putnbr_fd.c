aa4ef399c5b1b10e5695447642ececb430c5ce80
rubii <gamez.zeta@gmail.com>
2022-02-20 11:24:33 +0000

    putnbr fixed

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -15,13 +15,17 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
+	{
 		write(fd, "-2147483648", 11);
+		return ;
+	}
 	if (n < 0)
 	{
-		write(fd, '-', 1);
+		ft_putchar_fd('-', fd);
 		n *= -1;
 	}
 	if (n >= 10)
-		ft_putchar_fd(n / 10, fd);
-	write(fd, (n % 10) + '0', 1);
+		ft_putnbr_fd(n / 10, fd);
+	n %= 10;
+	ft_putchar_fd(n + '0', fd);
 }
