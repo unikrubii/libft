757f69e4e086aff9a0b072ec965ed200f8b6dabf
unikrubii <gamez.zeta@gmail.com>
2022-02-20 18:01:01 +0700

    putend

--- libft.h
+++ libft.h
@@ -40,6 +40,7 @@ void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *s, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
