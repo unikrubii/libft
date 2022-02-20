a32211d0b8ca1b2ac9027f8ab3ab6fdec767cc45
unikrubii <gamez.zeta@gmail.com>
2022-02-20 23:32:56 +0700

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 20:45:40 by sthitiku          #+#    #+#             */
+/*   Updated: 2022/02/20 20:45:40 by sthitiku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char		*result;
+
+	if (!s || !f)
+		return (NULL);
+	i = ft_strlen(s);
+	result = (char *)malloc(sizeof(char) * (i + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		result[i] = f(i, s[i]);
+	}
+	result = '\0';
+	return (result);
+}
