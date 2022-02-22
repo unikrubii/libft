7d9edb278ff973f02f0c1766cd63e6a3048c13a8
unikrubii <gamez.zeta@gmail.com>
2022-02-22 19:53:22 +0700

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -45,7 +45,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	first = first_i(s1, set);
 	last = last_i(s1, set);
-	size = last - first;
+	size = last - first + 1;
 	i = 0;
 	if (size == 0 && !s1 && !set)
 	{
