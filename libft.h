9357b12355f017c0fd846b4553c9aed1cfe2397c
unikrubii <gamez.zeta@gmail.com>
2022-02-22 19:02:27 +0700

    strtrim

--- libft.h
+++ libft.h
@@ -39,6 +39,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strtrim(char const *s1, char const *set);
 void	ft_bzero(void *s, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
